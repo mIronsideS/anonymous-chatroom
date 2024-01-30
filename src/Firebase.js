c6fd415cc15f762d64208cd59b2622f725ff80f6
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2024-01-30 17:01:18 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,24 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
-
-	apiKey: "AIzaSyDYf4krfOwKFj7Dx7HxraWpc0sfKeO-wlo",
-
+  apiKey: "AIzaSyDYf4krfOwKFj7Dx7HxraWpc0sfKeO-wlo",
   authDomain: "new-chat-ddbc2.firebaseapp.com",
-
   projectId: "new-chat-ddbc2",
-
   storageBucket: "new-chat-ddbc2.appspot.com",
-
   messagingSenderId: "388935952541",
-
   appId: "1:388935952541:web:e8492553f82b4959908c74"
-
-	
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-export { db, auth };
+
+// Create Google provider
+const googleAuthProvider = new GoogleAuthProvider();
+
+export { db, auth, googleAuthProvider };
